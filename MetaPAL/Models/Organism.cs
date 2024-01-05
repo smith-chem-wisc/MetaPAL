11616762bf784f64e04e04e0ffa5a7e09ebfa8c9
Zhuoxin Shi <zshi94@wisc.edu>
2024-01-04 18:13:16 -0600

    Created and tested organism model and file reader

--- MetaPAL/Models/Organism.cs
+++ MetaPAL/Models/Organism.cs
@@ -1,6 +1,16 @@
-﻿namespace MetaPAL.Models
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace MetaPAL.Models
 {
     public class Organism
     {
+        [Key]
+        [Name("Proteome Id")]
+        public string ProteomeId { get; set; }
+        [Name("Organism")]
+        public string Name { get; set; }
+        [Name("Organism Id")]
+        public int OrganismId { get; set; }
     }
 }
---
+++ Test/TestOrganisms.cs
@@ -0,0 +1,34 @@
+﻿using MetaPAL.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class TestOrganisms
+    {
+        [Test]
+        public void TestOrganismFileLoading()
+        {
+            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Organism.tsv");
+            var file = new OrganismFile(path);
+            file.LoadResults();
+
+            var organisms = file.Results;
+            Assert.That(organisms.Count(), Is.EqualTo(25));
+
+            var first = organisms.First();
+            Assert.That(first.ProteomeId, Is.EqualTo("UP000000625"));
+            Assert.That(first.Name, Is.EqualTo("Escherichia coli (strain K12)"));
+            Assert.That(first.OrganismId, Is.EqualTo(83333));
+
+            var last = organisms.Last();
+            Assert.That(last.ProteomeId, Is.EqualTo("UP000000515"));
+            Assert.That(last.Name, Is.EqualTo("Drosophila x virus (isolate Chung/1996) (DXV)"));
+            Assert.That(last.OrganismId, Is.EqualTo(654931));
+
+        }
+    }
+}
